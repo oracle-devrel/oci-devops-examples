e66b1e954d815c63e1ae472d86d3b366c7e50ad0
fahdabidiroottenancy/Rahul <test@example.com>
2022-04-22 13:58:27 +0530

    added more statements

--- oci-config-examples/oci_devops_policy_dg_terraform/terraform/compartment_scope/policies.tf
+++ oci-config-examples/oci_devops_policy_dg_terraform/terraform/compartment_scope/policies.tf
@@ -1,9 +1,12 @@
 resource "oci_identity_policy" "devopspolicy" {
+  provider       = oci.home_region
   name           = "${var.compartment_name}_devopspolicies"
   description    = "policy created for devops"
   compartment_id = var.compartment_ocid
 
   statements = [
-    "Allow ${var.compartment_name}_devops_dg to manage all-resources in compartment id ${var.compartment_ocid}"
+    "Allow group Administrators to manage devops-family in compartment id ${var.compartment_ocid}",
+    "Allow group Administrators to manage all-artifacts in compartment id ${var.compartment_ocid}",
+    "Allow  dynamic-group devops_dg_${var.compartment_name} to manage all-resources in compartment id ${var.compartment_ocid}",
   ]
 }
--- oci-config-examples/oci_devops_policy_dg_terraform/terraform/tenancy_scope/dgs.tf
+++ oci-config-examples/oci_devops_policy_dg_terraform/terraform/tenancy_scope/dgs.tf
@@ -1,6 +1,6 @@
 resource "oci_identity_dynamic_group" "devopsgroup1" {
   provider       = oci.home_region
-  name           = "${var.compartment_name}_devops_dg"
+  name           = "devops_dg_${var.compartment_name}"
   description    = "DevOps deploy pipeline dynamic group"
   compartment_id = var.tenancy_ocid
   matching_rule  = "ALL {resource.type = 'devopsdeploypipeline', resource.compartment.id = '${var.compartment_ocid}'}"
